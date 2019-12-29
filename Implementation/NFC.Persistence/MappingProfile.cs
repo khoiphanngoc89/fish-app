7e39091373ddfcb830eb3f3d5b4e120d4aca2526
Khoi Phan <khoiphanngoc89@gmail.com>
2019-12-29 11:33:39 +0700

    fix: upload image

--- Implementation/NFC.Persistence/MappingProfile.cs
+++ Implementation/NFC.Persistence/MappingProfile.cs
@@ -1,16 +1,25 @@
 ﻿using AutoMapper;
 using NFC.Domain.Entities;
 using NFC.Persistence.Contracts;
+using NFC.Persistence.Helpers;
 using System.Linq;
 
 namespace NFC.Persistence
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <seealso cref="AutoMapper.Profile" />
     public class MappingProfile : Profile
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MappingProfile"/> class.
+        /// </summary>
         public MappingProfile()
         {
             CreateMap<ProductDto, Product>().ReverseMap();
             CreateMap<Menu, MenuDto>()
+                .ForMember(dest => dest.Image, opts => opts.MapFrom(src => FileHelper.GetImagePath(src.Image)))
                 .ForMember(dest => dest.SubMenus, opts => opts.MapFrom(src => src.SubMenus.Select(n => new SubMenuDto
                 {
                     Id = n.Id,
