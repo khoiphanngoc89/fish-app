9ee6aa3f6bc34b4e2654b5fb0834e7438c56e684
Khoi Phan <khoiphanngoc89@gmail.com>
2019-12-31 21:33:20 +0700

    fix: remove error

--- Implementation/NFC.WebAPI/Startup.cs
+++ Implementation/NFC.WebAPI/Startup.cs
@@ -7,7 +7,6 @@ using NFC.Api.Config.Automapper;
 using NFC.Api.Config.Cors;
 using NFC.Api.Config.Swagger;
 using NFC.Api.Config.Validators;
-using NFC.Application.Context;
 using NFC.Application.DependencyManager;
 
 namespace NFC.WebAPI
