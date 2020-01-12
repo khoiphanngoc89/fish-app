2d5959ce05aeef8619f40c0e0f8ac48b62497e9b
Khoi Phan <khoiphanngoc89@gmail.com>
2020-01-12 22:24:43 +0700

    feat: update service firebase

---
+++ Implementation/NFC.Persistence/Services/FireBaseService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NFC.Persistence.Services
+{
+    public class FireBaseService
+    {
+        private static readonly string apiKey = "AIzaSyDfLO47zzdk3Ayz_X3kJhTIdH-p-CB0AEo";
+        private static readonly string bucket = "fish-d3e4f.appspot.com";
+        private static readonly string authScheme = "hachiphan05@gmail.com";
+        private static readonly string authPassword = "@Ubm-09-09-1028";
+    }
+}
