2a1b87db23f2f02ad751ec216c1c105eb717181e
Khoi Phan <khoiphanngoc89@gmail.com>
2020-02-29 11:49:47 +0700

    fix: clean code

--- Implementation/nfc.client/src/middleware/auth-route.js
+++ Implementation/nfc.client/src/middleware/auth-route.js
@@ -2,7 +2,6 @@ import router from '@/router/index.js';
 import store from '@/store/index.js';
 import { LOGIN_URL, ADMIN_ROOT_URL, DASHBOARD_URL } from '@/utils/constants/url.constant.js';
 const isAuthenticated = store.getters.isAuthenticated;
-//import storageUtil from '@/utils/localStorage.util';
 
 function toLoginPage(to) {
   return to.fullPath.includes(LOGIN_URL);
@@ -11,8 +10,6 @@ function toLoginPage(to) {
 function toRequiredAuthPage (to) {
     return !toLoginPage(to);
 }
-
-
 
 function redirectToLogin(to, next) {
   next({
@@ -38,18 +35,14 @@ router.beforeEach((to, from, next) => {
       // route: access to admin/login but already authen -> redirect to dashboard
       if(isAuthenticated && toLoginPage(to)) {
         redirectToDashboard(to, next);
-      }
-      else if (!isAuthenticated && toRequiredAuthPage(to)) { // route: access to authen request page but not yet authen -> redirect to login
+      } else if (!isAuthenticated && toRequiredAuthPage(to)) { // route: access to authen request page but not yet authen -> redirect to login
         redirectToLogin(to, next);
-      } else { // process redirect to target page
-        next();
-      }
+      } else next(); // make sure to always call next()!
     } else if(isAuthenticated && isAdminArea(to)) { // route: access to  /admin and has authen -> direct doashboard
       redirectToDashboard(to, next);
     } else if(!isAuthenticated && isAdminArea(to)) { // route: access to /admin but not authen -> direct to login
       redirectToLogin(to, next);
-    }
-    else next(); // make sure to always call next()!
+    } else next(); // make sure to always call next()!
   })
 
 export default router;
