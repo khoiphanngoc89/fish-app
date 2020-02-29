74a734dd59d0b8b00328710779afb4beed174a9e
Khoi Phan <khoiphanngoc89@gmail.com>
2020-02-29 11:29:35 +0700

    feat: update router

--- Implementation/nfc.client/src/middleware/auth-route.js
+++ Implementation/nfc.client/src/middleware/auth-route.js
@@ -2,10 +2,17 @@ import router from '@/router/index.js';
 import store from '@/store/index.js';
 import { LOGIN_URL, ADMIN_ROOT_URL, DASHBOARD_URL } from '@/utils/constants/url.constant.js';
 const isAuthenticated = store.getters.isAuthenticated;
+//import storageUtil from '@/utils/localStorage.util';
 
-function notLogin (to) {
-    return !to.fullPath.includes(LOGIN_URL);
+function toLoginPage(to) {
+  return to.fullPath.includes(LOGIN_URL);
 }
+
+function toRequiredAuthPage (to) {
+    return !toLoginPage(to);
+}
+
+
 
 function redirectToLogin(to, next) {
   next({
@@ -21,22 +28,25 @@ function redirectToDashboard(to, next) {
 }
 
 function isAdminArea(to) {
-  return to.path.includes(ADMIN_ROOT_URL) && notLogin(to);
+  return to.path.includes(ADMIN_ROOT_URL) && toRequiredAuthPage(to);
 }
 
 router.beforeEach((to, from, next) => {
+    // this route requires auth, check if logged in
+    // if not, redirect to login page.
     if (to.matched.some(record => record.meta.requiresAuth)) {
-      // this route requires auth, check if logged in
-      // if not, redirect to login page.
-      if (!isAuthenticated && notLogin(to)) {
-        redirectToLogin(to, next);
+      // route: access to admin/login but already authen -> redirect to dashboard
+      if(isAuthenticated && toLoginPage(to)) {
+        redirectToDashboard(to, next);
       }
-      else {
+      else if (!isAuthenticated && toRequiredAuthPage(to)) { // route: access to authen request page but not yet authen -> redirect to login
+        redirectToLogin(to, next);
+      } else { // process redirect to target page
         next();
       }
-    } else if(isAuthenticated && isAdminArea(to)) { // users -> /admin then will check authen and redirect to 
+    } else if(isAuthenticated && isAdminArea(to)) { // route: access to  /admin and has authen -> direct doashboard
       redirectToDashboard(to, next);
-    } else if(!isAuthenticated && isAdminArea(to)) {
+    } else if(!isAuthenticated && isAdminArea(to)) { // route: access to /admin but not authen -> direct to login
       redirectToLogin(to, next);
     }
     else next(); // make sure to always call next()!
